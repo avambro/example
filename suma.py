9810ce477d378e7ad0e29b938bd8a51751900326
Carlos Avalos <avambro@gmail.com>
2024-08-17 15:24:22 -0600

    methodo calculadora

--- suma.py
+++ suma.py
@@ -7,3 +7,10 @@ def resta (a,b):
 def multiplicacion(a, b):
     return a * b
 
+def calculadora():
+    print("Bienvenido a la calculadora básica")
+    print("Seleccione la operación:")
+    print("1. Suma")
+    print("2. Resta")
+    print("3. Multiplicación")
+    print("4. División")
